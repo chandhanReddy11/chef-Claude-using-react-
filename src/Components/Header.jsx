7ed9000a424d5bf066f5214f0e5882dbbb6bd35c
Baireddy Sai Chandan Reddy <luckybaireddy@gmail.com>
2025-02-19 17:35:55 +0530

    Your commit message here

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,9 @@ import chefClaudeLogo from 'D:/React/chef-claude/src/assets/chef-claude-icon.png
 export default function Header(){
     return (
         <header>
-        <img src={chefClaudeLogo} alt="" />
+        <img src={chefClaudeLogo} alt="" onMouseLeave={()=>{
+            alert("chef claude is waiting for you");
+        }} />
         <h1>Chef Claude</h1>
         </header>
     )
--- src/Components/Mai.jsx
+++ src/Components/Mai.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 export default function Main(){
+    const [ingredients,setIngredients]=useState([]);
+    function handleSubmit(event){
+        event.preventDefault();
+        const formdata=new FormData(event.currentTarget);
+        setIngredients([...ingredients,formdata.get("ingredients")]);
+        event.currentTarget.reset();
+        
+    }
+    
+    function Add({props}){
+        return(
+            <> 
+        {props.map((item, index) => (
+            <li key={index}>{item}</li> // Adding key for React list rendering
+        ))}
+        </>)
+    }
+   
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. Biryani" aria-label="e.g. Biryani" />
-                <button >+ Add-Ingredient</button>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input type="text" name="ingredients" placeholder="e.g. Biryani" aria-label="e.g. Biryani" />
+                <button type="submit">+ Add-Ingredient</button>
+               
             </form>
+            <ul>
+                <Add props={ingredients}/>
+                </ul>
         </main>
     )
 
