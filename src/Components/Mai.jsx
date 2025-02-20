67c1adca179ccd9687a6160f04455a9d35e9b71d
Baireddy Sai Chandan Reddy <luckybaireddy@gmail.com>
2025-02-20 22:15:29 +0530

    Your commit message here

--- src/Components/Mai.jsx
+++ src/Components/Mai.jsx
@@ -2,33 +2,29 @@ import { useState } from "react";
 
 export default function Main(){
     const [ingredients,setIngredients]=useState([]);
-    function handleSubmit(event){
-        event.preventDefault();
-        const formdata=new FormData(event.currentTarget);
-        setIngredients([...ingredients,formdata.get("ingredients")]);
-        event.currentTarget.reset();
-        
+    function handleSubmit(formData){
+        setIngredients((ingredients)=>{return [...ingredients,formData.get("ingredients")]});
     }
     
-    function Add({props}){
+    function Add(props){
         return(
             <> 
-        {props.map((item, index) => (
-            <li key={index}>{item}</li> // Adding key for React list rendering
-        ))}
-        </>)
+                {props.props.map((item, index) => (
+                    <li key={index}>{item}</li> // Adding key for React list rendering
+            ))}
+            </>)
     }
    
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={handleSubmit} className="add-ingredient-form">
                 <input type="text" name="ingredients" placeholder="e.g. Biryani" aria-label="e.g. Biryani" />
-                <button type="submit">+ Add-Ingredient</button>
-               
+                <button type="submit">+ Add-Ingredient</button> 
             </form>
             <ul>
-                <Add props={ingredients}/>
-                </ul>
+                    <Add props={ingredients}/>
+                </ul>  
+            
         </main>
     )
 
